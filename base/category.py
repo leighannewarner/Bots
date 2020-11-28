cda25578ec40ac0b7aad81def765801efe5bef90
Leigh Anne <leighannewarner@gmail.com>
2020-11-27 22:01:55 -0800

    Clean up category and reply bot
    Set up to convert betty

--- base/category.py
+++ base/category.py
@@ -1,33 +1,40 @@
 # base/category.py
 import random
 
-from enum import Enum
+from enum import IntEnum
 from fuzzywuzzy import fuzz
 
 
-class MatchType(Enum):
+class MatchType(IntEnum):
     PARTIAL = 0  # Recommended for longer phrases
     FULL = 1  # Recommended for individual words and shorter phrases
+
+
+class MatchPriority(IntEnum):
+    LOW = 0
+    MEDIUM = 1
+    HIGH = 2
 
 
 class Category:
 
     FUZZ_RATIO = 85
 
-    def __init__(self, category_name):
+    def __init__(self, category_name, match_type, match_priority):
         self.category_name = category_name
-        self.match_type = MatchType.PARTIAL
+        self.match_type = match_type
+        self.match_priority = match_priority
         self.phrase_list = []
         self.response_list = []
-
-    def set_match_type(self, phrase_type):
-        self.match_type = phrase_type
 
     def add_phrases(self, phrase_list):
         self.phrase_list += phrase_list
 
     def add_responses(self, response_list):
         self.response_list += response_list
+
+    def get_name(self):
+        return self.get_name()
 
     def choose_response(self):
         if not self.response_list:
@@ -38,19 +45,20 @@ class Category:
     def has_match(self, message_text):
         for phrase in self.phrase_list:
             if self.match_type is MatchType.PARTIAL:
-                return self.__phrase_match(phrase, message_text)
+                if self.__phrase_match(phrase, message_text):
+                    print('Matched "' + phrase + '" in category ' + self.category_name)
+                    return True
             if self.match_type is MatchType.FULL:
-                return self.__full_match(phrase, message_text)
+                if self.__phrase_match(phrase, message_text):
+                    print('Matched "' + phrase + '" in category ' + self.category_name)
+                    return True
+        return False
 
     def __phrase_match(self, phrase, message_text):
-        if fuzz.partial_ratio(message_text.lower(), phrase.lower()) >= self.FUZZ_RATIO:
-            print('Matched "' + phrase + '" in category ' + self.category_name)
-            return True
-        return False
+        return fuzz.partial_ratio(message_text.lower(), phrase.lower()) >= self.FUZZ_RATIO
 
     def __full_match(self, phrase, message_text):
         for word in message_text.split(' '):
             if fuzz.ratio(word.lower(), phrase.lower()) >= self.FUZZ_RATIO:
-                print('Matched "' + phrase + '" in category ' + self.category_name)
                 return True
         return False
--- base/reply_bot.py
+++ base/reply_bot.py
@@ -14,18 +14,17 @@ class ReplyBot(bot_base.BaseBot):
         if not self.can_reply(message):
             return
 
-        category = self.__get_match_category(message)
-        if not category:
+        matched_categores = self.__get_match_categories(message)
+        if not matched_categores:
+            print('No matched categories')
             return
 
-        response = self.choose_response(category)
-        print('Chose ' + response)
+        response = self.choose_response(matched_categores)
         await self.send_response(message, response)
 
     # CONDITIONS  -----------------------------------------------------------------------------------------------------
-    def add_category(self, name, match_type):
-        category = Category(name)
-        category.set_match_type(match_type)
+    def add_category(self, name, match_type, match_priority):
+        category = Category(name, match_type, match_priority)
         self.categories[name] = category
 
     def add_phrases(self, category_name, phrase_list):
@@ -34,10 +33,12 @@ class ReplyBot(bot_base.BaseBot):
     def add_responses(self, category_name, response_list):
         self.categories[category_name].add_responses(response_list)
 
-    def choose_response(self, category):
-        return category.choose_response()
+    def choose_response(self, matched_categores):
+        return matched_categores[0].choose_response()
 
-    def __get_match_category(self, message):
+    def __get_match_categories(self, message):
+        matched_categores = []
         for category in self.categories.values():
             if category.has_match(message.content):
-                return category
+                matched_categores.append(category)
+        return sorted(matched_categores, key=lambda value: value.match_priority, reverse=True)
