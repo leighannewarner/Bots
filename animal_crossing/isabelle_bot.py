99518583929423d36d025e95a97fb4da12536a10
leighannewarner <leighannewarner@gmail.com>
2021-02-02 22:23:03 -0800

    Make Isabelle bot do basic turnip things

--- animal_crossing/isabelle_bot.py
+++ animal_crossing/isabelle_bot.py
@@ -2,9 +2,12 @@
 
 import os
 import re
+import datetime
 
 from base import bot_base
 from dotenv import load_dotenv
+from animal_crossing import turnip_database
+from datetime import timedelta
 
 load_dotenv()
 
@@ -15,11 +18,17 @@ class IsabelleBot(bot_base.BaseBot):
         super().__init__(os.getenv('ISABELLE_CLIENT_TOKEN'))
         self.add_channel('animal-crossing')
 
+    async def on_ready(self):
+        await super().on_ready()
+        for channel in self.get_all_channels():
+            if channel.name in self.active_channels:
+                await channel.send('Hello there, it\'s me, Isabelle!')
+
     async def on_message(self, message):
         if not self.can_reply(message):
             return
 
-        message_content = self.strip_alerts(message.content)
+        message_content = strip_alerts(message.content)
         if not message_content:
             message_content = ''
 
@@ -32,18 +41,15 @@ class IsabelleBot(bot_base.BaseBot):
             return
 
         if re.match(r'turnips?', message_params[1]):
+            reply = 'I\'m sorry, something went wrong...'
             if message_params[0] == 'sell':
-                self.write_sell_price(message.author.id, message_params[1])
-                await self.send_response(message, 'I\'m still learning how to help you with that')
-                return
+                reply = write_sell_price(message.author.id, message_params[2])
             elif message_params[0] == 'buy':
-                self.write_buy_price(message.author.id, message_params[1])
-                await self.send_response(message, 'I\'m still learning how to help you with that')
-                return
+                reply = write_buy_price(message.author.id, message_params[2])
             elif message_params[0] == 'predict':
-                self.write_predict_price(message.author.id, message_params[1])
-                await self.send_response(message, 'I\'m still learning how to help you with that')
-                return
+                reply = get_prediction(message.author.id)
+            await self.send_response(message, reply)
+            return
 
         await self.print_error(message)
 
@@ -77,17 +83,88 @@ class IsabelleBot(bot_base.BaseBot):
                                  + 'Please tell me once more. '
                                  + 'If you\'re really stuck, try asking `@Isabelle` for `help`!')
 
-    def write_buy_price(self, id, price):
-        return
 
-    def write_sell_price(self, id, price):
-        return
+def write_buy_price(user_id, price):
+    if most_recent_sunday() is None:
+        return 'I don\'t think that\'s quite right...'
+    turnip_database.write_buy_price(user_id, price, date_fmt(most_recent_sunday()))
+    return 'Playing the market this week? Good luck!'
 
-    def get_prediction(self, id):
-        return
 
-    def strip_alerts(self, message):
-        matches = re.match(r'(!ac.?|<@!\d{18}>)(.*$\b)', message)
-        if matches:
-            return matches.group(2)
+def write_sell_price(user_id, price):
+    if normalized_sell_date() is None:
+        return 'I don\'t think that\'s quite right...'
+    turnip_database.write_sell_price(user_id, price, normalized_sell_date())
+    return 'Ok, all set.'
+
+
+def get_prediction(user_id):
+    sunday = most_recent_sunday()
+
+    buy_row = turnip_database.get_buy_row(user_id, date_fmt(sunday))
+    sell_rows = []
+
+    for i in range(1, 7):
+        today = sunday + timedelta(days=i)
+        sell_rows.append(turnip_database.get_sell_row(user_id, datetime_fmt_am(today)))
+        sell_rows.append(turnip_database.get_sell_row(user_id, datetime_fmt_pm(today)))
+
+    buy_param = buy_row[1]
+    if buy_param is None:
+        buy_param = ''
+
+    sell_param = ''
+    for row in sell_rows:
+        if row is None:
+            sell_param += '.'
+            continue
+        sell_param += f'.{row[1]}'
+
+    return f'I\'m still learning, consult an expert for now: https://turnipprophet.io?prices={buy_param}{sell_param}'
+
+
+def most_recent_sunday():
+    sunday = datetime.datetime.now()
+
+    loop_cap = 1
+    while sunday.weekday() != 6:
+        loop_cap += 1
+        sunday = sunday - timedelta(days=1)
+        if loop_cap > 7:
+            return None
+    return sunday
+
+
+def date_fmt(date):
+    return date.strftime("%Y-%m-%d")
+
+
+def datetime_fmt(date):
+    if date.hour < 12 or date.weekday() == 6:
+        return datetime_fmt_am(date)
+    return datetime_fmt_pm(date)
+
+
+def datetime_fmt_am(date):
+    return date.strftime("%Y-%m-%d:AM")
+
+
+def datetime_fmt_pm(date):
+    return date.strftime("%Y-%m-%d:PM")
+
+
+def normalized_sell_date():
+    date = datetime.datetime.now()
+
+    # Can't update sell price on Sundays
+    if date.weekday() == 6:
         return None
+
+    return datetime_fmt(date)
+
+
+def strip_alerts(message):
+    matches = re.match(r'(!ac.?|<@!\d{18}>)(.*$\b)', message)
+    if matches:
+        return matches.group(2)
+    return None
--- discord_database.py
+++ discord_database.py
@@ -93,4 +93,5 @@ def drop_database():
     conn.close()
 
 
-main()
+if __name__ == "__main__":
+    main()
